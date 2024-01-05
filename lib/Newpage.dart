eeca8e5e82237246ec381c2dea65f3aedc073ea2
Maansi <maansigarg06@gmail.com>
2024-01-05 21:40:43 +0530

    till removeP

--- lib/Newpage.dart
+++ lib/Newpage.dart
@@ -1,6 +1,8 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
+import 'package:uuid/uuid.dart';
 const names=[
   'alice',
   'bob',
@@ -52,4 +54,58 @@ class stlnew extends ConsumerWidget {
     );
   }
 }
+@immutable
+class Person {
+  final String name;
+  final int age;
+  final String uuid;
 
+  Person({
+    required this.name,
+    required this.age,
+    String? uuid,
+  }):uuid=uuid??const Uuid().v4();
+
+  Person Updated([String?name,int?age])=>Person(
+      name: name??this.name,
+      age: age??this.age,
+      uuid:uuid,
+  );
+  String get displayName =>'$name ($age year old)';
+
+  @override
+  bool operator ==(covariant Person other)=>uuid==other.uuid;
+
+  @override
+  int get hashCode=>uuid.hashCode;
+
+  @override
+  String toString()=> 'Person(name:$name,age:$age,uuid:$uuid)';
+}
+class DataModel extends ChangeNotifier{
+  final List<Person> _people=[];
+  int get count => _people.length;
+  UnmodifiableListView<Person> get people=> UnmodifiableListView(_people);
+  void add(Person person){
+    _people.add(person);
+    notifyListeners();
+  }
+  void remove(Person person){
+    _people.remove(person);
+    notifyListeners();
+  }
+  void update(Person updatedPerson){
+    final index =_people.indexOf(updatedPerson);
+    final oldPerson = _people[index];
+  }
+}
+class exp5 extends ConsumerWidget {
+  const exp5({Key? key}) :super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+
+    );
+  }
+}
